05513395cd85f1ff7532b487c24cdce40220c14b
Christian Mortaro <christian.mortaro@gmail.com>
2023-03-09 21:44:40 -0300

    :construction: optimize restart

--- src/loaders/inject_restart_to_module.rs
+++ src/loaders/inject_restart_to_module.rs
@@ -1,17 +1,18 @@
 use swc_common::DUMMY_SP;
 use swc_core::ecma::{
     ast::*,
+    atoms::JsWord,
     visit::{noop_visit_mut_type, VisitMut, VisitMutWith},
 };
 
 #[derive(Default)]
 pub struct InjectRestartVisitor {
-    nullstack: Option<Ident>,
-    starter: Option<Ident>,
-    starter_path: Option<String>,
+    nullstack: Option<JsWord>,
+    starter: Option<JsWord>,
+    starter_path: Option<JsWord>,
 }
 
-fn runtime_restart(starter_path: String) -> ModuleItem {
+fn runtime_restart(starter_path: &JsWord) -> ModuleItem {
     ModuleItem::Stmt(Stmt::Expr(ExprStmt {
         span: DUMMY_SP,
         expr: Box::new(Expr::Call(CallExpr {
@@ -40,7 +41,11 @@ fn runtime_restart(starter_path: String) -> ModuleItem {
                 },
                 ExprOrSpread {
                     spread: None,
-                    expr: Box::new(Expr::Lit(Lit::Str(starter_path.into()))),
+                    expr: Box::new(Expr::Lit(Lit::Str(Str {
+                        span: DUMMY_SP,
+                        value: starter_path.into(),
+                        raw: None,
+                    }))),
                 },
             ],
             type_args: None,
@@ -59,7 +64,7 @@ impl VisitMut for InjectRestartVisitor {
         // find application path
         n.visit_mut_children_with(self);
         if let Some(starter_path) = &self.starter_path {
-            n.body.push(runtime_restart(starter_path.clone()));
+            n.body.push(runtime_restart(starter_path));
         }
     }
 
@@ -68,12 +73,12 @@ impl VisitMut for InjectRestartVisitor {
             if let Callee::Expr(c) = &n.callee {
                 if let Expr::Member(expr) = &**c {
                     if let Some(ident) = expr.clone().obj.ident() {
-                        if ident.sym == nullstack.sym {
+                        if ident.sym == *nullstack {
                             if let MemberProp::Ident(prop) = &expr.prop {
                                 if prop.sym.eq("start") {
                                     if let Some(spread) = n.args.first() {
                                         if let Some(ident) = spread.expr.clone().ident() {
-                                            self.starter = Some(ident)
+                                            self.starter = Some(ident.sym)
                                         }
                                     }
                                 }
@@ -90,19 +95,19 @@ impl VisitMut for InjectRestartVisitor {
             if n.src.value.eq("nullstack") {
                 for specifier in n.specifiers.iter() {
                     if let ImportSpecifier::Default(ident) = specifier {
-                        self.nullstack = Some(ident.local.clone())
+                        self.nullstack = Some(ident.local.sym.clone())
                     }
                 }
             }
         } else if let Some(starter) = &self.starter {
             n.specifiers.iter().find(|specifier| {
                 let local = match specifier {
-                    ImportSpecifier::Named(s) => &s.local,
-                    ImportSpecifier::Default(s) => &s.local,
-                    ImportSpecifier::Namespace(s) => &s.local,
+                    ImportSpecifier::Named(s) => &s.local.sym,
+                    ImportSpecifier::Default(s) => &s.local.sym,
+                    ImportSpecifier::Namespace(s) => &s.local.sym,
                 };
-                if local.sym.clone() == starter.sym {
-                    self.starter_path = Some(n.src.value.to_string())
+                if *local == *starter {
+                    self.starter_path = Some(n.src.value.clone())
                 }
                 self.starter_path.is_some()
             });
